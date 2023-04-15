738a4295195d1dc4f34b57f3533002933a7fcb05
psychomet <sanatkar.dev@gmail.com>
2023-04-15 15:50:13 +0330

    feat(showcase): remove nx-welcome comp

--- packages/showcase/src/app/app.component.ts
+++ packages/showcase/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
+  imports: [],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-  title = 'showcase';
-}
+export class AppComponent {}
