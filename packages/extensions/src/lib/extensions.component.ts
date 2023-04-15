b0fb6de1401c1e7f1cf494ef1816bd529c065cbc
psychomet <sanatkar.dev@gmail.com>
2023-04-15 13:08:47 +0330

    feat(extensions): initial commit

---
+++ packages/extensions/src/lib/extensions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-extensions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './extensions.component.html',
+  styleUrls: ['./extensions.component.less'],
+})
+export class ExtensionsComponent {}
