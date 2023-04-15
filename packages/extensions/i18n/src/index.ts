e11b85f4e11fd5d1da63bab692a367d0df1eea66
psychomet <sanatkar.dev@gmail.com>
2023-04-15 13:48:03 +0330

    feat(extensions): completed core and i18n libs

--- packages/extensions/i18n/src/index.ts
+++ packages/extensions/i18n/src/index.ts
@@ -1,1 +1,15 @@
-export * from './lib/i18n.module';
+/**
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
+ */
+
+export * from './lib/nz-i18n.module';
+export * from './lib/nz-i18n.service';
+export * from './lib/date-config';
+export * from './lib/nz-i18n.interface';
+export * from './lib/nz-i18n.token';
+export * from './lib/nz-i18n.pipe';
+export * from './lib/date-helper.service';
+
+export { default as fa_IR } from './lib/languages/fa_IR';
+export { default as en_US } from './lib/languages/en_US';
